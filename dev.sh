3f2da8b105ad4f6fc08a0da591695e5b79abd0cb
David Polák <git@davidpolak.cz>
2025-10-26 15:23:27 +0100

    56a57f90-b277-11f0-87a6-f02f7420c3f1

---
+++ dev.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+rm ~/.factorio/mods/meltea-s-early-transport-belt-capacity_0.1.0.zip; \
+cp -r early-transport-belt-capacity /tmp/meltea-s-early-transport-belt-capacity_0.1.0/ \
+&& 7z a \
+  ~/.factorio/mods/meltea-s-early-transport-belt-capacity_0.1.0.zip \
+  /tmp/meltea-s-early-transport-belt-capacity_0.1.0 \
+&& rm -rf /tmp/meltea-s-early-transport-belt-capacity_0.1.0
+
+
